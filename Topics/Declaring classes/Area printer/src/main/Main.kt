380ec425c51a4400cdf22c1cd77716f4a82c662a
Mogakamo <mogaka.amo254@gmail.com>
2021-08-04 17:17:52 +0300

    finished with classes

--- Topics/Declaring classes/Area printer/src/main/Main.kt
+++ Topics/Declaring classes/Area printer/src/main/Main.kt
@@ -2,6 +2,6 @@ class Rectangle(val width: Int, val height: Int)
 
 fun printArea(rectangle: Rectangle) {
     // ...
-    val rectangle: Rectangle = Rectangle()
-
+    val rectangle: Rectangle = Rectangle(21, 12)
+     println(rectangle.width * rectangle.height)
 }
